9a0c092fd27491b45bb1b6a343f7e2431b3cd761
tellapurivenkatesh123 <tellapurivenkatesh6635@gmail.com>
2025-07-24 18:55:06 +0530

    initial commit

---
+++ stacks.java
@@ -0,0 +1,55 @@
+// class Node{
+//     int data;
+//     Node next;
+//     Node(int data){
+//         this.data=data;
+//         this.next=null;
+//     }
+// }
+// public class stacks {
+//     public static Node top;
+//     public static void insert(int data){
+//         Node newnode=new Node(data);
+//         if(top==null){
+//             top=newnode;
+//             return;
+//         }
+//         newnode.next=top;
+//         top=newnode;
+//     }
+//     public static void print(){
+//         Node node=top;
+//         if(node==null)throw new IllegalArgumentException("stack is empty/underflow");
+//         while(node!=null){
+//             System.out.print(node.data+" -->");
+//             node=node.next;
+//         }
+//         System.out.println("null");
+//     }
+// public static void remove(){
+//     if(top==null)throw new IllegalArgumentException("stsck is empty/underflow");
+//     top=top.next;
+// }
+// public static void length(){
+//     if(top==null)throw new IllegalArgumentException("stack is empty");
+//     Node node=top;
+//     int i=1;
+//     while(node.next!=null){
+//         i++;
+//         node=node.next;
+//     }
+//     System.out.println("length->>"+i);
+// }
+// public static void main(String args[]){
+// // top=new Node(1);
+// // top.next= new Node(2);
+// // top.next.next=new Node(3);
+// insert(1);
+// insert(2);
+// insert(3);
+// length();
+// remove();
+// length();
+// print();
+// }
+// }
